fdb61cdf59ede9ce1983a863f4b451a553c9bc10
Alan Naranjo <lamjo30@gmail.com>
2023-01-16 09:38:43 -1100

    Api

---
+++ src/components/ApiOne.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import axios from 'axios';
+  import { ref } from 'vue';
+
+  let datos: any = ref();
+  let number: any = ref();
+
+  axios.get('https://api.chucknorris.io/jokes/random')
+    .then((response) => {
+      response.data
+      console.log(response.data);
+      datos = response.data
+    })
+
+</script> 
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-auto text-center">
+        <h1>Datos de NASA</h1>
+      </div>
+      <div class="col-auto text-center">
+        <li v-for="element in datos">
+          {{ element.id }}
+          <img src={{ element.icon_url }} alt=""/>
+        </li>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
+  import TheWelcome from '../components/TheWelcome.vue'
+  import ApiOne from '@/components/ApiOne.vue';
+
+</script>
 <template>
   <main>
-    
+    <ApiOne/>
   </main>
 </template>
