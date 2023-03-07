ba6e612a680e66d2d795817854b5499bea44b131
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-03-07 21:41:32 +0100

    ty

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
-<template>
-  <h1>WELCOME A VUE3</h1>
-  <RouterView />
-</template>
